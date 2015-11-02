c42c0aab1620f8d32b3d0c47600d9c926012cb77
Luis-Miguel Rodriguez Rojas <lmr3@cee-ktk3-asrv1.ce.gatech.edu>
2015-11-02 16:09:45 -0500

    Minor typo

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -50,7 +50,7 @@ class PasswordResetsController < ApplicationController
      # Confirms a valid user.
      def valid_user
 	unless (@user && @user.activated? &&
-	      @user.authenticated?(:reset, params[:id])))
+	      @user.authenticated?(:reset, params[:id]))
 	   redirect_to root_url
 	end
      end
