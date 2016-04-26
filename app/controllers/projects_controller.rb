994e6e5ad04f0d8b78e677fc9e2fce04630d96d1
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-04-26 13:13:33 -0400

    Code-style up

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,43 +2,52 @@ class ProjectsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :destroy, :show, :result,
     :reference_datasets, :show_dataset, :reference_dataset_result,
     :medoid_clade]
+  
   before_action :admin_user, only: [:new, :create, :destroy, :new_ncbi_download,
     :create_ncbi_download, :start_daemon, :stop_daemon]
 
+  # Initiate (paginated) list of projects.
   def index
     @projects = Project.paginate(page: params[:page])
   end
-   
+  
+  # Create an empty abstract project.
   def new
     @project = Project.new
   end
-
+  
+  # Loads a given project.
   def show
     @project = Project.find(params[:id])
   end
-
+  
+  # Loads (paginated) list of reference datasets in a project.
   def reference_datasets
     @project = Project.find(params[:project_id])
-    current_page = (params[:page] || 1).to_i
+    cur_page = (params[:page] || 1).to_i
     per_page = (params[:per_page] || 30).to_i
     @ref_datasets = @project.ref_datasets
-    @datasets = WillPaginate::Collection.create(current_page, per_page, @ref_datasets.size) do |pager|
-      start = (current_page-1)*per_page
+    @datasets = WillPaginate::Collection.create(
+                        cur_page, per_page, @ref_datasets.size) do |pager|
+      start = (cur_page - 1) * per_page
       pager.replace(@ref_datasets[start, per_page])
     end
   end
-
+  
+  # Loads a medoid clade for in-page display.
   def medoid_clade
     @project = Project.find(params[:project_id])
     redirect_to root_url if @project.miga.nil?
     @metric = params[:metric].to_sym
-    @result = @project.miga.add_result(@metric==:AAI ? :clade_finding : :subclades, false)
+    @result = @project.miga.add_result(
+      @metric==:AAI ? :clade_finding : :subclades, false)
     @clade = params[:clade]
     @clade_name = "#{@metric} clade #{@clade}"
     @clade_miga = @clade.split("-").
       map{ |i| "miga-project.1.sc-#{i}" }.join("/")
   end
-
+  
+  # Loads a medoid clade for asynchronous display.
   def medoid_clade_as
     medoid_clade
     render partial: "medoid_clade",
@@ -47,29 +56,32 @@ class ProjectsController < ApplicationController
 	root_name: @clade },
       layout: false
   end
-
+  
+  # Loads a given dataset in a project.
   def show_dataset
     @project = Project.find(params[:id])
     @dataset_miga = @project.miga.dataset(params[:dataset])
     redirect_to root_url unless @dataset_miga.is_ref?
   end
-   
+  
+  # Create project.
   def create
     @user = User.find_by(id: params[:user_id])
     @project = @user.projects.create(project_params)
     if @project.save and not @project.miga.nil?
-	 [:name,:description,:comments,:type].each do |k|
-	    @project.miga.metadata[k] = params[k] unless
-	       params[k].nil? or params[k].empty?
-	 end
-	 @project.miga.save
-	 flash[:success] = "Project created."
-	 redirect_to @project
+      [:name,:description,:comments,:type].each do |k|
+        @project.miga.metadata[k] = params[k] unless
+          params[k].nil? or params[k].empty?
+      end
+      @project.miga.save
+      flash[:success] = "Project created."
+      redirect_to @project
     else
 	 render "new"
     end
   end
-
+  
+  # Destroys project.
   def destroy
     project = Project.find(params[:id])
     FileUtils.rm_rf project.miga.path
@@ -77,35 +89,39 @@ class ProjectsController < ApplicationController
     flash[:success] = "Project deleted"
     redirect_to root_url
   end
-
+  
+  # Loads a result of a project.
   def result
     if p = Project.find(params[:id]) and m = p.miga
-	 if res = m.result(params[:result])
-	    if file = res.data[:files][params[:file].to_sym]
-	       send_result(file, res)
-	       return
-	    end
-	 end
+      if res = m.result(params[:result])
+        if file = res.data[:files][params[:file].to_sym]
+          send_result(file, res)
+          return
+        end
+      end
     end
     render :nothing => true, :status => 200, :content_type => "text/html"
   end
 
+  # Loads a result from a reference dataset in a project.
   def reference_dataset_result
     if p = Project.find(params[:id]) and m = p.miga
-	 if ds = m.dataset(params[:dataset]) and res = ds.result(params[:result])
-	    if file = res.data[:files][params[:file].to_sym]
-	       send_result(file, res)
-	       return
-	    end
-	 end
+      if ds = m.dataset(params[:dataset]) and res = ds.result(params[:result])
+        if file = res.data[:files][params[:file].to_sym]
+          send_result(file, res)
+          return
+        end
+      end
     end
     render :nothing => true, :status => 200, :content_type => "text/html"
   end
 
+  # Initiates project for NCBI download.
   def new_ncbi_download
-    @project = Project.find(params[:id])
+    show
   end
-   
+  
+  # Launches an NCBI download in the background.
   def create_ncbi_download
     @project = Project.find(params[:project_id])
     species = params[:species] || ""
@@ -114,19 +130,22 @@ class ProjectsController < ApplicationController
     codes += "40|" if params[:chromosome]
       
     if codes.empty? or species.empty?
-	 flash[:danger] = "Nothing to do, please set at least one status to download." if codes.empty?
-	 flash[:danger] = "Nothing to do, please specify a species name." if species.empty?
-	 render "new_ncbi_download"
+      flash[:danger] = "Nothing to do, " +
+        "please set at least one status to download." if codes.empty?
+      flash[:danger] = "Nothing to do, " +
+        "please specify a species name." if species.empty?
+      render "new_ncbi_download"
     else
-	 if @project.ncbi_download!(species, codes)
-	    flash[:success] = "Downloading reference genomes in the background..."
-	    redirect_to @project
-	 else
-	    render "new_ncbi_download"
-	 end
+      if @project.ncbi_download!(species, codes)
+        flash[:success] = "Downloading reference genomes in the background..."
+        redirect_to @project
+      else
+        render "new_ncbi_download"
+      end
     end
   end
-
+  
+  # Launch daemon in the background.
   def start_daemon
     require "miga/daemon"
     @project = Project.find(params[:id])
@@ -138,7 +157,8 @@ class ProjectsController < ApplicationController
     Spawnling.new { daemon.start }
     redirect_to @project
   end
-
+  
+  # Stops a daemon running in the background.
   def stop_daemon
     require "miga/daemon"
     @project = Project.find(params[:id])
@@ -152,29 +172,32 @@ class ProjectsController < ApplicationController
   private
 
     def project_params
-	 params.require(:project).permit(:path)
+      params.require(:project).permit(:path)
     end
 
     def send_result(file, res)
-	 f = File.expand_path(file, res.dir)
-	 if Dir.exist? f and params[:f] and not params[:f]=~/\//
-	    f = File.expand_path(params[:f], f)
-	    file = params[:f]
-	 end
-	 if Dir.exist? f
-	    @path = f
-	    @file = file
-	    @res = res
-	    render template: "shared/result_dir"
-	 else
-	    case File.extname(file)
-	    when ".pdf"
-	       send_file(f, filename:file, disposition:"inline", type:"application/pdf", x_sendfile:true)
-	    when ".html"
-	       send_file(f, filename:file, disposition:"inline", type:"text/html", x_sendfile:true)
-	    else
-	       send_file(f, filename:file, disposition:"inline", type:"raw/text", x_sendfile:true)
-	    end
-	 end
+      f = File.expand_path(file, res.dir)
+      if Dir.exist? f and params[:f] and not params[:f]=~/\//
+        f = File.expand_path(params[:f], f)
+        file = params[:f]
+      end
+      if Dir.exist? f
+        @path = f
+        @file = file
+        @res = res
+        render template: "shared/result_dir"
+      else
+        case File.extname(file)
+        when ".pdf"
+          send_file(f, filename:file, disposition:"inline",
+            type:"application/pdf", x_sendfile:true)
+        when ".html"
+          send_file(f, filename:file, disposition:"inline",
+            type:"text/html", x_sendfile:true)
+        else
+          send_file(f, filename:file, disposition:"inline",
+            type:"raw/text", x_sendfile:true)
+        end
+      end
     end
 end
--- app/controllers/query_datasets_controller.rb
+++ app/controllers/query_datasets_controller.rb
@@ -1,129 +1,136 @@
 class QueryDatasetsController < ApplicationController
-   before_action :logged_in_user, only: [:index, :new, :create, :show, :destroy, :result]
-   before_action :correct_user_or_admin, only: [:show, :destroy, :result]
+  before_action :logged_in_user, only: [:index, :new, :create, :show, :destroy,
+    :result]
+  
+  before_action :correct_user_or_admin, only: [:show, :destroy, :result]
 
-   def index
-      # Find query datasets
-      if params[:project_id]
-	 @project = Project.find(params[:project_id])
-	 qd = params[:all] ?
-	    @project.query_datasets.all :
-	    QueryDataset.by_user_and_project(current_user, @project)
-      else
-	 @project = nil
-	 qd = params[:all] ?
-	    QueryDataset.all :
-	    current_user.query_datasets
-      end
-      @all_qd = qd.count
-      params[:ready] ||= false
-      if params[:ready]=="yes"
-	 qd = qd.select{ |qd| qd.ready? }
-	 @ready_qd = qd.count
-	 @running_qd = @all_qd - @ready_qd
-      elsif params[:ready]=="no"
-	 qd = qd.select{ |qd| not qd.ready? }
-	 @running_qd = qd.count
-	 @ready_qd = @all_qd - @running_qd
-      else
-	 @ready_qd = qd.select{ |qd| qd.ready? }.count
-	 @running_qd = @all_qd - @ready_qd
-      end
-      # Paginate
-      current_page = (params[:page] || 1).to_i
-      per_page = (params[:per_page] || 30).to_i
-      @query_datasets = WillPaginate::Collection.create(current_page, per_page, qd.size) do |pager|
-	 start = (current_page-1)*per_page
-	 pager.replace(qd[start, per_page])
-      end
-   end
+  def index
+    # Find query datasets
+    if params[:project_id]
+      @project = Project.find(params[:project_id])
+      qd = params[:all] ?
+        @project.query_datasets.all :
+        QueryDataset.by_user_and_project(current_user, @project)
+    else
+      @project = nil
+      qd = params[:all] ?
+        QueryDataset.all :
+        current_user.query_datasets
+    end
+    @all_qd = qd.count
+    params[:ready] ||= false
+    if params[:ready]=="yes"
+      qd = qd.select{ |qd| qd.ready? }
+      @ready_qd = qd.count
+      @running_qd = @all_qd - @ready_qd
+    elsif params[:ready]=="no"
+      qd = qd.select{ |qd| not qd.ready? }
+      @running_qd = qd.count
+      @ready_qd = @all_qd - @running_qd
+    else
+      @ready_qd = qd.select{ |qd| qd.ready? }.count
+      @running_qd = @all_qd - @ready_qd
+    end
+    # Paginate
+    cur_page = (params[:page] || 1).to_i
+    per_page = (params[:per_page] || 30).to_i
+    @query_datasets = WillPaginate::Collection.create(cur_page, per_page,
+                            qd.size) do |pager|
+      start = (cur_page-1)*per_page
+      pager.replace(qd[start, per_page])
+    end
+  end
    
-   def new
-      @project = Project.find_by(id: params[:project_id])
-      if @project.nil?
-	 redirect_to projects_path
+  def new
+    @project = Project.find_by(id: params[:project_id])
+    if @project.nil?
+      redirect_to projects_path
+    else
+      @query_dataset = QueryDataset.new
+    end
+  end
+
+  def show
+    @query_dataset = QueryDataset.find(params[:id])
+  end
+
+  def create
+    @project = Project.find_by(id: query_dataset_params[:project_id])
+    if @project.nil?
+      redirect_to root_url
+    else
+      @query_dataset = @project.query_datasets.create(query_dataset_params)
+      flash[:success] = "It's saved" if @query_dataset.save!
+      if @query_dataset.save and not @query_dataset.miga.nil?
+        [:description,:comments,:type].each do |k|
+          @query_dataset.miga.metadata[k] = params[k] unless
+            params[k].nil? or params[k].empty?
+        end
+        @query_dataset.miga.save
+        flash[:success] = "Query dataset created."
+        redirect_to @query_dataset
       else
-	 @query_dataset = QueryDataset.new
+        params[:project_id] = query_dataset_params[:project_id]
+        flash[:danger] = "Query dataset couldn't be saved."
+        render "new"
       end
-   end
+    end
+  end
 
-   def show
-      @query_dataset = QueryDataset.find(params[:id])
-   end
+  def destroy
+    qd = QueryDataset.find(params[:id])
+    p = qd.project
+    u = qd.user
+    p.miga.unlink_dataset qd.miga.name
+    qd.miga.remove!
+    qd.destroy
+    redirect_to u
+  end
 
-   def create
-      @project = Project.find_by(id: query_dataset_params[:project_id])
-      if @project.nil?
-	 redirect_to root_url
-      else
-	 @query_dataset = @project.query_datasets.create(query_dataset_params)
-	 flash[:success] = "It's saved" if @query_dataset.save!
-	 if @query_dataset.save and not @query_dataset.miga.nil?
-	    [:description,:comments,:type].each do |k|
-	       @query_dataset.miga.metadata[k] = params[k] unless
-		  params[k].nil? or params[k].empty?
-	    end
-	    @query_dataset.miga.save
-	    flash[:success] = "Query dataset created."
-	    redirect_to @query_dataset
-	 else
-	    params[:project_id] = query_dataset_params[:project_id]
-	    flash[:danger] = "Query dataset couldn't be saved."
-	    render "new"
-	 end
+  def result
+    if qd = QueryDataset.find(params[:id]) and m = qd.miga
+      if res = m.result(params[:result])
+        if file = res.data[:files][params[:file].to_sym]
+          f = File.expand_path(file, res.dir)
+          if Dir.exist? f and params[:f] and not params[:f]=~/\//
+            f = File.expand_path(params[:f], f)
+            file = params[:f]
+          end
+          if Dir.exist? f
+            @path = f
+            @file = file
+            @res = res
+            render template: "shared/result_dir"
+          else
+            case File.extname(f)
+            when ".pdf"
+              send_file(f, filename:file, disposition:"inline",
+                type:"application/pdf", x_sendfile:true)
+            when ".html"
+              send_file(f, filename:file, disposition:"inline",
+                type:"text/html", x_sendfile:true)
+            else
+              send_file(f, filename:file, disposition:"inline",
+                type:"raw/text", x_sendfile:true)
+            end
+          end
+          return
+        end
       end
-   end
+    end
+    render :nothing => true, :status => 200, :content_type => "text/html"
+  end
 
-   def destroy
-      qd = QueryDataset.find(params[:id])
-      p = qd.project
-      u = qd.user
-      p.miga.unlink_dataset qd.miga.name
-      qd.miga.remove!
-      qd.destroy
-      redirect_to u
-   end
+  private
 
-   def result
-      if qd = QueryDataset.find(params[:id]) and m = qd.miga
-	 if res = m.result(params[:result])
-	    if file = res.data[:files][params[:file].to_sym]
-	       f = File.expand_path(file, res.dir)
-	       if Dir.exist? f and params[:f] and not params[:f]=~/\//
-		  f = File.expand_path(params[:f], f)
-		  file = params[:f]
-	       end
-	       if Dir.exist? f
-		  @path = f
-		  @file = file
-		  @res = res
-		  render template: "shared/result_dir"
-	       else
-		  case File.extname(f)
-		  when ".pdf"
-		     send_file(f, filename:file, disposition:"inline", type:"application/pdf", x_sendfile:true)
-		  when ".html"
-		     send_file(f, filename:file, disposition:"inline", type:"text/html", x_sendfile:true)
-		  else
-		     send_file(f, filename:file, disposition:"inline", type:"raw/text", x_sendfile:true)
-		  end
-	       end
-	       return
-	    end
-	 end
-      end
-      render :nothing => true, :status => 200, :content_type => "text/html"
-   end
-
-   private
-
-      def query_dataset_params
-	 params.require(:query_dataset).permit(:name, :user_id, :project_id, :input_file, :input_file_2, :input_type)
-      end
+    def query_dataset_params
+      params.require(:query_dataset).permit(
+        :name, :user_id, :project_id, :input_file, :input_file_2, :input_type)
+    end
       
-      # Confirms the correct user
-      def correct_user_or_admin
-	 @user = QueryDataset.find(params[:id]).user
-	 redirect_to(root_url) unless current_user?(@user) or current_user.admin?
-      end
+    # Confirms the correct user
+    def correct_user_or_admin
+      @user = QueryDataset.find(params[:id]).user
+      redirect_to(root_url) unless current_user?(@user) or current_user.admin?
+    end
 end
