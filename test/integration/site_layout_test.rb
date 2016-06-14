f15dd05ba7c65f44b13b92a88b7fab7367467bc2
Luis-Miguel Rodriguez-R <lmrodriguezr@gmail.com>
2016-06-14 17:19:35 -0400

    test fixed

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,7 +5,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
       get root_path
       assert_template 'static_pages/home'
       assert_select "a[href=?]", root_path, count: 2
-      assert_select "a[href=?]", help_path
       assert_select "a[href=?]", about_path
       assert_select "a[href=?]", contact_path
    end
