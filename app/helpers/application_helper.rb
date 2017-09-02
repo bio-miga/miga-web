07163822ad37ba8e6dfbaa150c2adb27229b9897
Luis-Miguel Rodriguez-R <lmrodriguezr@gmail.com>
2017-09-02 18:27:11 -0400

    Plot of assembly statistics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -43,6 +43,14 @@ module ApplicationHelper
   def breadcrumb(location="")
     (location.empty? ? "" : ("/ " + location + "")).html_safe
   end
+
+  def plotly(data, layout={})
+    id = SecureRandom.uuid
+    content_tag(:div, id:id) do
+      javascript_tag "Plotly.plot('#{id}', [#{data.to_json}], " +
+        "#{layout.to_json});"
+    end
+  end
    
   def plot_distances(file, opts={}, layout={})
     x = []
@@ -58,15 +66,42 @@ module ApplicationHelper
   end
    
   def filled_area_plot(x, y, opts={}, layout={})
-    id = SecureRandom.uuid
     data = opts
     data[:fill] ||= "tozeroy"
     data[:type] ||= "scatter"
     data[:x] = x
     data[:y] = y
-    content_tag(:div, id:id) do
-      javascript_tag "Plotly.plot('#{id}', [#{data.to_json}], " +
-        "#{layout.to_json});"
-    end
+    plotly(data, layout)
+  end
+
+  def plot_assembly(n50, len)
+    # Trace1: RefSeq genomes
+    trace1 = {
+      x: [298471,2240716,3402093,4653910,13033779],
+      type: "box",
+      hoverinfo: "none",
+      name: "RefSeq"
+    }
+    # Layout
+    layout = {
+      showlegend: false,
+      xaxis: {
+        autorange: true,
+        zeroline: false,
+        title: "Length (bp)"
+      },
+      annotations: []
+    }
+    layout[:annotations] << {
+      x: len, y: 0.25, text: "Total length",
+      showarrow: true, yanchor: "bottom",
+      ax: 0, ay: -40
+    }
+    layout[:annotations] << {
+      x: n50, y: 0.25, text: "N50",
+      showarrow: true, yanchor: "bottom",
+      ax: 0, ay: -20
+    } unless n50==len
+    plotly(trace1, layout)
   end
 end
